4394c120c397f010da2fe6e8f6ea63f1add813e3
Ghaith Tarawneh <gtarawneh@gmail.com>
2016-03-25 22:35:50 +0100

    Refactor and cleanup

--- LanguageTool.py
+++ LanguageTool.py
@@ -4,17 +4,16 @@ import xml.etree.ElementTree
 import urllib
 import urllib2
 
+# problems is an array of n-tuples in the form (x0, x1, msg, description, suggestion)
+problems = []
 
-# mistakes is an array of n-tuples in the form (x0, x1, msg, description, suggestion)
-mistakes = []
+# index of currently selected problem
+selProblem = 0;
 
-# index of currently selected mistake
-selMistake = 0;
-
-# highlight all mistakes
-def outlineMistakes():
-	for c in mistakes:
-		highlight(c[0], c[1]);
+# outline all problems
+def outlineProblems():
+	for c in problems:
+		outline(c[0], c[1]);
 
 # select characters with indeces [i, j]
 def moveCaret(self, i, j):
@@ -22,43 +21,43 @@ def moveCaret(self, i, j):
 	self.view.sel().clear()
 	self.view.sel().add(sublime.Region(target, target+j-i))
 
-# select mistake i
+# select problem i
 def selectProblem(self, i):
-	if len(mistakes) > 0:
-		c = mistakes[i];
-		outlineMistakes();
+	if len(problems) > 0:
+		c = problems[i];
+		outlineProblems();
 		moveCaret(self, c[0], c[1]);
 		sublime.status_message(c[3] + " (" + c[4] + ")")
 	else:
 		sublime.status_message("run language check first")		
 
-class moveNextMistakeCommand(sublime_plugin.TextCommand):
+class gotoNextProblemCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		global mistakes
-		global selMistake
+		global problems
+		global selProblem
 
-		selMistake += 1
-		if selMistake>=len(mistakes):
-			selMistake = 0;
+		selProblem += 1
+		if selProblem>=len(problems):
+			selProblem = 0;
 
-		selectProblem(self, selMistake);
+		selectProblem(self, selProblem);
 
-class movePrevMistakeCommand(sublime_plugin.TextCommand):
+class gotoPrevProblemCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		global mistakes
-		global selMistake
+		global problems
+		global selProblem
 
-		selMistake -= 1
-		if selMistake<0:
-			selMistake = len(mistakes) - 1;
+		selProblem -= 1
+		if selProblem<0:
+			selProblem = len(problems) - 1;
 
-		selectProblem(self, selMistake);
+		selectProblem(self, selProblem);
 
-class HelloWorldCommand(sublime_plugin.TextCommand):
+class LanguageToolCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		global mistakes
-		global selMistake
-		mistakes = [];
+		global problems
+		global selProblem
+		problems = [];
 		v = self.view;
 		str1 = v.substr(sublime.Region(0, v.size()));
 		self.view.replace(edit, sublime.Region(0, self.view.size()), str1);
@@ -75,58 +74,17 @@ class HelloWorldCommand(sublime_plugin.TextCommand):
 				category = child.attrib["category"]
 				msg = child.attrib["msg"]
 				replacements = child.attrib["replacements"]
-				mistakes.append((x1,x2, category, msg, replacements));
-				highlight(x1, x2);
+				problems.append((x1,x2, category, msg, replacements));
+				outline(x1, x2);
 				errors_found = True;
 		if errors_found:
-			selMistake = 0;
-			selectProblem(self, selMistake);
+			selProblem = 0;
+			selectProblem(self, selProblem);
 		else:
 			sublime.status_message("No errors found :-)");
 
-
-	def run_old(self, edit):
-		#self.view.insert(edit, 0, "hey there!")
-		v = self.view;
-		str1 = v.substr(sublime.Region(0, v.size()));
-		# print str1
-		d = parseContent(str1);
-		for word in d:
-			if (d[word]>1) and (len(word)>3):
-				print("removing: " + word + " (" + str(d[word]) + " occurrenecs)")
-				inds = findSubstr(str1, word);
-				for i in inds:
-					highlight(i, i+len(word), 0);
-				# str1 = str1.replace(word, '?' * len(word));
-		# self.view.replace(edit, sublime.Region(0, self.view.size()), str1);
-
-def highlight(x0, x1):
+def outline(x0, x1):
 	r = sublime.Region(x0, x1);
 	v = sublime.active_window().active_view()
 	flags = sublime.DRAW_OUTLINED;
 	obj1 = v.add_regions("key_test" + str(x0), [r], "comment", "", flags);
-
-def findSubstr(str, substr):
-	return [m.start() for m in re.finditer(substr, str)]
-
-def parseContent(str):
-
-	lineArr = str.split("\n");
-	f = lambda str : str.rstrip()
-	d = dict();
-
-	for ln in lineArr:
-		ln2 = ln.rstrip().split(' ');
-		for word in ln2:
-			if word in d:
-				d[word] += 1
-			else:
-				d[word] = 1
-
-	return d
-
-# interesting:
-
-# this is how to grab some input from the user (return type is object, not sure what that is exactly)
-
-# x = sublime.active_window().show_input_panel("How is it going:", "", 0, None, None)	
