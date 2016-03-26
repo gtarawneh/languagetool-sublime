5802c8ca7a14ce6e48c229fec8e3b7bde399eb49
Ghaith Tarawneh <gtarawneh@gmail.com>
2016-03-26 17:33:25 +0000

    Make navigation relative to caret position

--- LanguageTool.py
+++ LanguageTool.py
@@ -1,16 +1,17 @@
+# LanguageTool.py
+#
+# This is a simple Sublime Text plugin for checking grammar. it passes buffer
+# content to LanguageTool (via http) and highlights reported problems.
+
 import sublime, sublime_plugin
-import re
 import xml.etree.ElementTree
 import urllib
-import urllib2
 
-# problems is an array of n-tuples in the form (x0, x1, msg, description, suggestion)
+# problems is an array of n-tuples in the form
+# (x0, x1, msg, description, suggestions, regionID)
 problems = []
 
-# index of currently selected problem
-selProblem = 0
-
-# select characters with indeces [i, j]
+# select characters with indices [i, j]
 def moveCaret(self, i, j):
 	target = self.view.text_point(0, i)
 	self.view.sel().clear()
@@ -20,60 +21,85 @@ def moveCaret(self, i, j):
 def getProbKey(i):
 	return "p" + str(i)
 
+# wrapper
+def msg(str):
+	sublime.status_message(str)
+
 def clearRegions(self):
 	v = self.view;
-	for i in range(0, len(problems)-1):
+	for i in range(0, len(problems)):
 		v.clear_region(getProbKey(i))
 
-# select problem i, assumes len(problems)>0
-def selectProblem(self, i):
+def selectProblem(self, p):
 	v = self.view
-	if len(problems) > 0:
-		c = problems[i]
-		regions = v.get_regions(getProbKey(i))
-		r = regions[0]
-		moveCaret(self, r.a, r.b)
-		sublime.status_message(c[3] + " (" + c[4] + ")")
+	r = v.get_regions(p[5])[0]
+	moveCaret(self, r.a, r.b)
+	if len(p[4])>0:
+		msg(p[3] + " (" + p[4] + ")")
+	else:
+		msg(p[3])			
+
+# check if string contains any unicode chars
+def containsUnicode(str):
+	try:
+		str.decode('ascii') 
+		return False
+	except UnicodeEncodeError:
+		return True
+
+def problemSolved(self, p):
+	r = self.view.get_regions(p[5])[0]
+	return (r.a == r.b)
 
 # navigation function
 class gotoNextProblemCommand(sublime_plugin.TextCommand):
 	def run(self, edit, jumpSizeStr):
 		global problems
-		global selProblem
-
 		if len(problems) > 0:
 			jumpSize = int(jumpSizeStr)
-			selProblem = (selProblem + jumpSize) % len(problems)
-			selectProblem(self, selProblem)
-		else:
-			sublime.status_message("run language check first")
+			caretPos = self.view.sel()[0].begin()
+			probInds = range(0, len(problems))
+			if jumpSize>0:
+				for ind in probInds: # forward search
+					p = problems[ind]
+					if (not problemSolved(self, p)) and (p[0] > caretPos):
+						selectProblem(self, p)
+						return
+			else:
+				for ind in reversed(probInds): # backward search
+					p = problems[ind]
+					if (not problemSolved(self, p)) and (p[0] < caretPos):
+						selectProblem(self, p)
+						return
+		msg("no language problems to fix")
 
 class LanguageToolCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		global problems
-		global selProblem
 		problems = []
 		v = self.view
 		clearRegions(self)
 		strText = v.substr(sublime.Region(0, v.size()))
-		urlargs = urllib.urlencode({'language' : 'en-US', 'text': strText})
-		s = "http://localhost:8081/?" + urlargs
-		content = urllib2.urlopen(s).read()
-		print(content)
-		root = xml.etree.ElementTree.fromstring(content)
-		ind = 0;
-		for child in root:
-			if child.tag == "error":
-				a = int(child.attrib["fromx"])
-				b = int(child.attrib["tox"])
-				category = child.attrib["category"]
-				msg = child.attrib["msg"]
-				replacements = child.attrib["replacements"]
-				v.add_regions(getProbKey(ind), [sublime.Region(a, b)], "comment", "", 0 * sublime.DRAW_OUTLINED)
-				problems.append((a, b, category, msg, replacements))
-				ind += 1;
-		if ind>0:
-			selProblem = 0
-			selectProblem(self, selProblem)
+		if containsUnicode(strText):
+			msg('text contains unicode, unable to pass to LanguageTool')
 		else:
-			sublime.status_message("No errors found :-)")
+			urlargs = urllib.urlencode({'language' : 'en-US', 'text': strText})
+			s = "http://localhost:8081/?" + urlargs
+			content = urllib.urlopen(s).read()
+			root = xml.etree.ElementTree.fromstring(content)
+			ind = 0;
+			for child in root:
+				if child.tag == "error":
+					a = int(child.attrib["fromx"])
+					b = int(child.attrib["tox"])
+					category = child.attrib["category"]
+					msg = child.attrib["msg"]
+					replacements = child.attrib["replacements"]
+					regionKey = getProbKey(ind)
+					v.add_regions(regionKey, [sublime.Region(a, b)], "string", "", sublime.DRAW_OUTLINED)
+					problems.append((a, b, category, msg, replacements, regionKey))
+					ind += 1
+			if ind>0:
+				selectProblem(self, problems[0])
+			else:
+				msg("no language problems were found :-)")
