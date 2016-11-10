gtarawneh/languagetool-sublime

commit 6c570cfa508e31d566a6f83dcef1d271dc601b3b

fixed imports to be py3 compatible

LTServer.py
@@ -1,5 +1,10 @@
 import sublime
-import urllib
+try:
+    from urlparse import urlencode
+    from urllib2 import urlopen
+except ImportError:
+    from urllib.parse import urlencode
+    from urllib.request import urlopen
 
 import sys
 import os
@@ -31,19 +36,19 @@ def _post(server, payload):
 		return _post_ST3(server, payload)
 
 def _post_ST2(server, payload):
-	data = urllib.urlencode(payload)
+	data = urlencode(payload)
 	try:
-		content = urllib.urlopen(server, data).read()
+		content = urlopen(server, data).read()
 	except IOError:
 		return None
 	else:
 		return content
 
 def _post_ST3(server, payload):
-	data = urllib.parse.urlencode(payload)
+	data = urlencode(payload)
 	data = data.encode('utf8')
 	try:
-		content = urllib.request.urlopen(server, data).read()
+		content = urlopen(server, data).read()
 	except IOError:
 		return None
 	else:

commit 6865edecb0df4ac7fcefeba97f2ca507c8347b65

Fix ST2 import issue introduced in commit 6c570cfa

LTServer.py
@@ -1,14 +1,17 @@
 import sublime
-try:
-    from urlparse import urlencode
-    from urllib2 import urlopen
-except ImportError:
-    from urllib.parse import urlencode
-    from urllib.request import urlopen
-
 import sys
 import os
 import json
+
+def _is_ST2():
+	return (int(sublime.version()) < 3000)
+
+if _is_ST2():
+	from urllib import urlencode
+	from urllib import urlopen
+else:
+	from urlparse import urlencode
+	from urllib2 import urlopen
 
 def getResponse(server, text, lang, ignoredIDs):
 	payload = {
@@ -25,9 +28,6 @@ def getResponse(server, text, lang, ignoredIDs):
 		return None
 
 # internal functions:
-
-def _is_ST2():
-	return (int(sublime.version()) < 3000)
 
 def _post(server, payload):
 	if _is_ST2():